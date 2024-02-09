00749c3c1fcdfaa70406c89380eede999c7df53c
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-02-09 14:39:38 -0300

    adicionar SpringSecurityConfig

---
+++ todo-management/todo-management/src/main/java/project/todomanagement/config/SpringSecurityConfig.java
@@ -0,0 +1,66 @@
+
+    package project.todomanagement.config;
+
+    import lombok.AllArgsConstructor;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.http.HttpMethod;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.config.Customizer;
+    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.core.userdetails.User;
+    import org.springframework.security.core.userdetails.UserDetails;
+    import org.springframework.security.core.userdetails.UserDetailsService;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.crypto.password.PasswordEncoder;
+    import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+    import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SpringSecurityConfig {
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("root")
+                .password(passwordEncoder().encode("root"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+               .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws
+            Exception {
+
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> {
+
+                    authorize.anyRequest()
+                            .authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+}
+
+
+
+
