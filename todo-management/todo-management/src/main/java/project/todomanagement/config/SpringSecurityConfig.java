612f20827c89164f685559a796743a8a02fc64e1
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-02-09 17:54:03 -0300

    Criar PasswordEncoder

--- todo-management/todo-management/src/main/java/project/todomanagement/config/SpringSecurityConfig.java
+++ todo-management/todo-management/src/main/java/project/todomanagement/config/SpringSecurityConfig.java
@@ -1,28 +1,29 @@
 
-    package project.todomanagement.config;
+package project.todomanagement.config;
 
-    import lombok.AllArgsConstructor;
-    import org.springframework.context.annotation.Bean;
-    import org.springframework.context.annotation.Configuration;
-    import org.springframework.http.HttpMethod;
-    import org.springframework.security.authentication.AuthenticationManager;
-    import org.springframework.security.config.Customizer;
-    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-    import org.springframework.security.core.userdetails.User;
-    import org.springframework.security.core.userdetails.UserDetails;
-    import org.springframework.security.core.userdetails.UserDetailsService;
-    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-    import org.springframework.security.crypto.password.PasswordEncoder;
-    import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-    import org.springframework.security.web.SecurityFilterChain;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableMethodSecurity
 @AllArgsConstructor
 public class SpringSecurityConfig {
+    private UserDetailsService userDetailsService;
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -30,18 +31,10 @@ public class SpringSecurityConfig {
 
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-               .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws
+            Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -59,7 +52,21 @@ public class SpringSecurityConfig {
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
+    @Bean
+    public UserDetailsService userDetailsService(){
 
+        UserDetails ramesh = User.builder()
+                .password(passwordEncoder().encode("root"))
+                 .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+          .username("admin")
+        .password(passwordEncoder().encode("admin"))
+        .roles("ADMIN")
+            .build();
+
+       return new InMemoryUserDetailsManager(ramesh, admin);
+      }
 }
 
 
