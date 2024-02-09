754ff2eb6766cfbc916e284110a7db945ab0f6e8
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-02-09 16:43:15 -0300

    Implementar a segurança em nivel de metodo

--- todo-management/todo-management/src/main/java/project/todomanagement/config/SpringSecurityConfig.java
+++ todo-management/todo-management/src/main/java/project/todomanagement/config/SpringSecurityConfig.java
@@ -52,12 +52,6 @@ public class SpringSecurityConfig {
         http.csrf((csrf) -> csrf.disable())
 
                 .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers(HttpMethod.POST, "api/**").hasRole("ADMIN");
-                    authorize.requestMatchers(HttpMethod.PUT, "api/**").hasRole("ADMIN");
-                    authorize.requestMatchers(HttpMethod.DELETE, "api/**").hasRole("ADMIN");
-                    authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN", "USER");
-                    authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN", "USER");
-                    authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();
 
                     authorize.anyRequest()
                             .authenticated();
--- todo-management/todo-management/src/main/java/project/todomanagement/controller/TodoController.java
+++ todo-management/todo-management/src/main/java/project/todomanagement/controller/TodoController.java
@@ -3,6 +3,7 @@ package project.todomanagement.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import project.todomanagement.dto.TodoDto;
 import project.todomanagement.service.TodoService;
@@ -14,38 +15,46 @@ import java.util.List;
 @AllArgsConstructor
 public class TodoController {
     private TodoService todoService;
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo( @RequestBody TodoDto todoDto) {
         return new ResponseEntity<>(todoService.addTodo(todoDto),
                 HttpStatus.CREATED);
     }
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping("{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long id) {
 
         return new ResponseEntity<>(todoService.getTodo(id),
                 HttpStatus.OK);
     }
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>> getAllTodos() {
         return new ResponseEntity<>(todoService.getAllTodos(),
                 HttpStatus.OK);
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable Long id) {
         todoService.removeTodo(id);
         return new ResponseEntity<>("Todo successfully deleted!", HttpStatus.OK);
 
+
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<TodoDto> updateTodo(@PathVariable Long id, @RequestBody TodoDto todoDto) {
         return new ResponseEntity<>(todoService.updateTodo(todoDto,id),
                 HttpStatus.OK);
     }
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/complete")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable Long id) {
         return new ResponseEntity<>(todoService.completeTodo(id),
                 HttpStatus.OK);
     }
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
     @PatchMapping("{id}/in-complete")
     public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable Long id) {
         return new ResponseEntity<>(todoService.inCompleteTodo(id),
